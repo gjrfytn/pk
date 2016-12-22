d2576b8bba9756f5fac756ca899520cf5a436d30
marie-giselle <uly00008@gmail.com>
2016-12-22 22:23:52 +0300

    Программа авторизации
    Заготовка программы. Выполняет авторизацию и открывает вкладку
    соответственно роли.
    +данные в таблице пользователей для проверки программы

---
+++ System/Autorization/Autorization/FormAuthorization.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Autorization
+{
+    public partial class FormAuthorization : Form
+    {
+        FormMain parent;
+        bool exitingApp = true;
+
+        public FormAuthorization(FormMain p)
+        {
+         parent = p;
+         InitializeComponent();
+        }
+
+        private void btAuth_Click(object sender, EventArgs e)
+        {
+            exitingApp = false;
+            for (int i = 0; i < usersBindingSource.Count; ++i)
+            {
+                pk_dbDataSet.usersRow row = (pk_dbDataSet.usersRow)(((DataRowView)usersBindingSource[i]).Row);
+                
+                if (cbLogin.Text == row.user_login)
+                {
+                    if (tbPassword.Text == row.user_password)
+                    {
+                        MessageBox.Show("Авторизация выполнена. Роль пользователя " + row.user_role + ".");
+                        switch (row.user_role)
+                        {
+                            case "registrator":
+                                this.parent.SetUsersRole(0);
+                                break;
+                            case "inspector":
+                                this.parent.SetUsersRole(1);
+                                break;
+                            case "administrator":
+                                this.parent.SetUsersRole(2);
+                                break;                            
+                        }
+                        this.parent.Show();
+                        this.Close();
+                    }
+                        
+                    else
+                        MessageBox.Show("Неверный пароль.");
+                    return;
+                }
+            }
+            MessageBox.Show("Логин не найден.");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            usersTableAdapter1.Fill(this.pk_dbDataSet.users);
+            cbLogin.DisplayMember = "user_login";
+        }
+
+        private void FormAuthorization_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (exitingApp) Application.Exit();
+        }
+    }
+}
---
+++ System/Autorization/Autorization/FormMain.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Data;
+using System.Windows.Forms;
+
+namespace Autorization
+{
+    public partial class FormMain : Form
+    {
+        public FormMain()
+        {
+            InitializeComponent();
+        }
+
+        int usersRole = 0;
+        bool justLoaded = true;
+
+        public void SetUsersRole (int role)
+        {
+            usersRole = role;
+        }
+
+        private void FormMain_Load(object sender, EventArgs e)
+        {            
+            FormAuthorization authForm = new FormAuthorization(this);
+            authForm.Show();
+        }
+
+        private void FormMain_Shown(object sender, EventArgs e)
+        {
+            if (justLoaded) this.Hide();
+        }
+
+        private void FormMain_VisibleChanged(object sender, EventArgs e)
+        {
+            tabControl1.SelectTab(usersRole);
+        }
+    }
+}
