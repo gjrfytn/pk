ac7bec54cf9ede6f630934b24acdc1dde4bd6616
Dmitry <dimon13142@mail.ru>
2017-04-02 20:05:32 +0300

    Доработка KLADR

--- System/PK/PK/Classes/KLADR.cs
+++ System/PK/PK/Classes/KLADR.cs
@@ -64,7 +64,7 @@ namespace PK.Classes
         public List<string> GetDistricts(string region)
         {
             string regCode = GetRegionCode(region);
-            if (regCode == "")
+            if (regCode == null)
                 return new List<string>();
 
             MySqlCommand cmd = new MySqlCommand("SELECT name, socr FROM subjects WHERE " + _WhereDistrict + " AND " + _RawRegionCode + "='" + regCode + "';", _Connection);
@@ -75,14 +75,14 @@ namespace PK.Classes
         public List<string> GetTowns(string region, string district)
         {
             string regCode = GetRegionCode(region);
-            if (regCode == "")
+            if (regCode == null)
                 return new List<string>();
 
             string distrCode;
             if (district != "")
             {
                 distrCode = GetDistrictCode(regCode, district);
-                if (distrCode == "")
+                if (distrCode == null)
                     return new List<string>();
             }
             else
@@ -101,6 +101,8 @@ namespace PK.Classes
 
             MySqlCommand cmd = new MySqlCommand("SELECT name, socr, `index` FROM subjects WHERE " + _WhereSettlement + " AND " + _RawRegionCode + "='" + regCode + "' AND " + _RawDistrictCode + "='" + distrCode + "' AND " + _RawTownCode + "='" + townCode + "';", _Connection);
 
+            /*т.к. в одном субъекте могут быть нас. п. с одинаковыми названиями и категориями,
+             мы прибавляем к одноимённым элементам их индекс.*/
             using (MySqlDataReader reader = cmd.ExecuteReader())
             {
                 List<string> results = new List<string>();
@@ -112,11 +114,11 @@ namespace PK.Classes
                 }
 
                 for (ushort i = 0; i < results.Count; ++i)
-                    if (results.Count(s => s == results[i]) > 1) //TODO Нужно?
+                    if (results.Count(s => s == results[i]) > 1) //TODO Нужно? Если элемент повторяется.
                     {
-                        for (ushort j = (ushort)(i + 1); j < results.Count; ++j)
+                        for (ushort j = (ushort)(i + 1); j < results.Count; ++j) //Ищем повторения (до него их быть не может).
                             if (results[i] == results[j])
-                                if (indexes[i] == indexes[j])
+                                if (indexes[i] == indexes[j]) //Если у них совпадают ещё и индексы, то остаётся только исключить один из них из списка.
                                 {
                                     results.RemoveAt(j);
                                     j--;
@@ -134,42 +136,17 @@ namespace PK.Classes
         public List<string> GetStreets(string region, string district, string town, string settlement)
         {
             string regCode, distrCode, townCode;
-            if (!GetCodes(region, district, town, out regCode, out distrCode, out townCode))
+            List<string> settlementsCodes;
+            if (!GetCodes(region, district, town, settlement, out regCode, out distrCode, out townCode, out settlementsCodes))
                 return new List<string>();
 
-            string settlCodesExpr = "";
-            List<string> settlementsCodes = new List<string>();
-            if (settlement != "")
-            {
-                settlementsCodes = GetSettlementCodes(regCode, distrCode, townCode, settlement);
-                if (settlementsCodes.Count == 0)
-                    return new List<string>();
-
-                settlCodesExpr += "(";
-                foreach (string code in settlementsCodes)
-                    settlCodesExpr += _RawSettlementCode + "='" + code + "' OR ";
-
-                settlCodesExpr = settlCodesExpr.Remove(settlCodesExpr.Length - 4);
-                settlCodesExpr += ")";
-            }
-            else
-                settlCodesExpr = _RawSettlementCode + "= '000'";
-
             string expr = "";
-            if (settlementsCodes.Count != 0)
+            foreach (string code in settlementsCodes)//Если нас. п. несколько, выбираем улицы из всех в один список. Предполагаем, что совпадения маловероятны.
             {
-                foreach (string code in settlementsCodes)
-                {
-                    string commonCode = regCode + distrCode + townCode + code;
-                    expr += "code BETWEEN '" + commonCode + "0000' AND '" + commonCode + "9999' OR ";
-                }
-                expr = expr.Remove(expr.Length - 4);
+                string commonCode = regCode + distrCode + townCode + code;
+                expr += "code BETWEEN '" + commonCode + "0000' AND '" + commonCode + "9999' OR ";
             }
-            else
-            {
-                string commonCode = regCode + distrCode + townCode + "000";
-                expr += "code BETWEEN '" + commonCode + "0000' AND '" + commonCode + "9999'";
-            }
+            expr = expr.Remove(expr.Length - 4);
 
             MySqlCommand cmd = new MySqlCommand("SELECT name, socr FROM streets WHERE " + expr + ";", _Connection);
 
@@ -179,59 +156,33 @@ namespace PK.Classes
         public List<string> GetHouses(string region, string district, string town, string settlement, string street)
         {
             string regCode, distrCode, townCode;
-            if (!GetCodes(region, district, town, out regCode, out distrCode, out townCode))
+            List<string> settlementsCodes;
+            if (!GetCodes(region, district, town, settlement, out regCode, out distrCode, out townCode, out settlementsCodes))
                 return new List<string>();
 
-            string settlCodesExpr = "";
-            List<string> settlementsCodes = new List<string>();
-            if (settlement != "")
-            {
-                settlementsCodes = GetSettlementCodes(regCode, distrCode, townCode, settlement);
-                if (settlementsCodes.Count == 0)
-                    return new List<string>();
-
-                foreach (string code in settlementsCodes)
-                    settlCodesExpr += "' AND " + _RawSettlementCode + "='" + code;
-            }
-            else
-                settlCodesExpr = "' AND " + _RawSettlementCode + "= '000";
-
-            string streetCode = "";
+            string streetCode = null;
             if (street != "")
             {
-                if (settlementsCodes.Count != 0)
+                foreach (string code in settlementsCodes)
                 {
-                    foreach (string code in settlementsCodes)
-                    {
-                        streetCode = GetStreetCode(regCode, distrCode, townCode, code, street);
-                        if (streetCode != "") //TODO Считаем, что нет улиц с одниковым названием в поселениях (вряд ли такое вообще может произойти).
-                            break;
-                    }
-
-                    if (streetCode == "")
-                        return new List<string>();
+                    streetCode = GetStreetCode(regCode, distrCode, townCode, code, street);
+                    if (streetCode != null)
+                        break; //Считаем, что нет улиц с одинаковым названием в нас. п., у которых тоже одинаковые названия.
                 }
-                else
-                    streetCode = GetStreetCode(regCode, distrCode, townCode, "000", street);
+
+                if (streetCode == null)
+                    return new List<string>();
             }
             else
                 streetCode = "0000";
 
             string expr = "";
-            if (settlementsCodes.Count != 0)
+            foreach (string code in settlementsCodes)
             {
-                foreach (string code in settlementsCodes)
-                {
-                    string commonCode = regCode + distrCode + townCode + code + streetCode;
-                    expr += "code BETWEEN '" + commonCode + "0000' AND '" + commonCode + "9999' OR ";
-                }
-                expr = expr.Remove(expr.Length - 4);
+                string commonCode = regCode + distrCode + townCode + code + streetCode;
+                expr += "code BETWEEN '" + commonCode + "0000' AND '" + commonCode + "9999' OR ";
             }
-            else
-            {
-                string commonCode = regCode + distrCode + townCode + "000" + streetCode;
-                expr += "code BETWEEN '" + commonCode + "0000' AND '" + commonCode + "9999'";
-            }
+            expr = expr.Remove(expr.Length - 4);
 
             MySqlCommand cmd = new MySqlCommand("SELECT name FROM houses WHERE " + expr + ";", _Connection);
 
@@ -248,22 +199,144 @@ namespace PK.Classes
             };
         }
 
+        public string GetIndex(string region, string district, string town, string settlement, string street, string house)
+        {
+            string index = null;
+            string regCode, distrCode, townCode;
+            List<string> settlementsCodes;
+            MySqlCommand cmd;
+            if (GetCodes(region, district, town, settlement, out regCode, out distrCode, out townCode, out settlementsCodes))
+            {
+                string streetCode = null;
+                if (street != "")
+                    foreach (string code in settlementsCodes)
+                    {
+                        streetCode = GetStreetCode(regCode, distrCode, townCode, code, street);
+                        if (streetCode != null)
+                            break; //Считаем, что нет улиц с одниковым названием в нас. п., у которых тоже одинаковые названия.
+                    }
+                else
+                    streetCode = "0000";
+
+                if (streetCode != null)
+                {
+                    if (house != "")
+                        foreach (string code in settlementsCodes)
+                        {
+                            cmd = new MySqlCommand(
+                             "SELECT `index` FROM houses WHERE `index` IS NOT NULL AND INSTR(name,'" + house + "') != 0 AND " +
+                             _RawRegionCode + " = '" + regCode + "' AND " +
+                             _RawDistrictCode + " = '" + distrCode + "' AND " +
+                             _RawTownCode + " = '" + townCode + "' AND " +
+                             _RawSettlementCode + " = '" + code + "' AND " +
+                             _RawStreetCode + " = '" + streetCode + "';",
+                             _Connection
+                             );
+
+                            index = SelectOneString(cmd); //TODO //Считаем, что запрос возвращает одну строку.
+                            if (index != null)//TODO
+                                return index;
+                        }
+
+                    foreach (string code in settlementsCodes)
+                    {
+                        cmd = new MySqlCommand(
+                         "SELECT `index` FROM streets WHERE `index` IS NOT NULL AND " +
+                         _RawRegionCode + " = '" + regCode + "' AND " +
+                         _RawDistrictCode + " = '" + distrCode + "' AND " +
+                         _RawTownCode + " = '" + townCode + "' AND " +
+                         _RawSettlementCode + " = '" + code + "' AND " +
+                         _RawStreetCode + " = '" + streetCode + "';",
+                         _Connection
+                         );
+
+                        index = SelectOneString(cmd);
+                        if (index != null)//TODO
+                            return index;
+                    }
+                }
+            }
+
+            if (settlementsCodes.Count != 0)
+                foreach (string code in settlementsCodes)
+                {
+                    cmd = new MySqlCommand(
+                     "SELECT `index` FROM subjects WHERE `index` IS NOT NULL AND " +
+                     _WhereSettlement + " AND " +
+                     _RawRegionCode + " = '" + regCode + "' AND " +
+                     _RawDistrictCode + " = '" + distrCode + "' AND " +
+                     _RawTownCode + " = '" + townCode + "' AND " +
+                     _RawSettlementCode + " = '" + code + "';",
+                     _Connection
+                     );
+
+                    index = SelectOneString(cmd);
+                    if (index != null)//TODO
+                        return index;
+                }
+
+            if (townCode != null)
+            {
+                cmd = new MySqlCommand(
+                    "SELECT `index` FROM subjects WHERE `index` IS NOT NULL AND " +
+                    _WhereTown + " AND " +
+                    _RawRegionCode + " = '" + regCode + "' AND " +
+                    _RawDistrictCode + " = '" + distrCode + "' AND " +
+                    _RawTownCode + " = '" + townCode + "';",
+                    _Connection
+                    );
+
+                index = SelectOneString(cmd);
+                if (index != null)//TODO
+                    return index;
+            }
+
+            if (distrCode != null)
+            {
+                cmd = new MySqlCommand(
+                    "SELECT `index` FROM subjects WHERE `index` IS NOT NULL AND " +
+                    _WhereDistrict + " AND " +
+                    _RawRegionCode + " = '" + regCode + "' AND " +
+                    _RawDistrictCode + " = '" + distrCode + "';",
+                    _Connection
+                    );
+
+                index = SelectOneString(cmd);
+                if (index != null)//TODO
+                    return index;
+            }
+
+            if (regCode != null)
+            {
+                cmd = new MySqlCommand(
+                    "SELECT `index` FROM subjects WHERE `index` IS NOT NULL AND " +
+                    _WhereRegion + " AND " +
+                    _RawRegionCode + " = '" + regCode + "';",
+                    _Connection
+                    );
+
+                index = SelectOneString(cmd);
+            }
+
+            return index;
+        }
+
         private bool GetCodes(string region, string district, string town, out string regCode, out string distrCode, out string townCode)
         {
             regCode = GetRegionCode(region);
-            if (regCode == "")
+            if (regCode == null)
             {
-                distrCode = "";
-                townCode = "";
+                distrCode = null;
+                townCode = null;
                 return false;
             }
 
             if (district != "")
             {
                 distrCode = GetDistrictCode(regCode, district);
-                if (distrCode == "")
+                if (distrCode == null)
                 {
-                    townCode = "";
+                    townCode = null;
                     return false;
                 }
             }
@@ -273,11 +346,29 @@ namespace PK.Classes
             if (town != "")
             {
                 townCode = GetTownCode(regCode, distrCode, town);
-                if (townCode == "")
+                if (townCode == null)
                     return false;
             }
             else
                 townCode = "000";
+
+            return true;
+        }
+
+        private bool GetCodes(string region, string district, string town, string settlement, out string regCode, out string distrCode, out string townCode, out List<string> settlementsCodes)
+        {
+            settlementsCodes = new List<string>();
+            if (!GetCodes(region, district, town, out regCode, out distrCode, out townCode))
+                return false;
+
+            if (settlement != "")
+            {
+                settlementsCodes = GetSettlementCodes(regCode, distrCode, townCode, settlement);
+                if (settlementsCodes.Count == 0)
+                    return false;
+            }
+            else
+                settlementsCodes.Add("000");
 
             return true;
         }
@@ -318,6 +409,9 @@ namespace PK.Classes
             return SelectOneString(cmd);
         }
 
+        /*Мы предполагаем, что у субъектов, высших по иерархии, не может быть одноимённых элементов.
+         Однако, у нас. п. такие совпадения возможны, поэтому эта функция возвращает список кодов
+         всех нас. п. с таким названием и индексом (если есть).*/
         private List<string> GetSettlementCodes(string regionCode, string distrCode, string townCode, string settlement)
         {
             string[] splitSettl = settlement.Split('(', ')', '[', ']');
@@ -357,7 +451,7 @@ namespace PK.Classes
         {
             string[] splitStreet = street.Split('(', ')');
             if (splitStreet.Length == 1)
-                return "";
+                return null;
 
             string name = splitStreet[0].Remove(splitStreet[0].Length - 1);
             string socr = splitStreet[1];
@@ -397,7 +491,7 @@ namespace PK.Classes
                     return reader.GetString(0);
                 }
                 else
-                    return "";
+                    return null;
             }
         }
     }
